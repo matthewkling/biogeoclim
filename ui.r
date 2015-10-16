41db488033a7d9d0051c158fd895d92eaeefbb83
Matthew Kling <mattkling@gmail.com>
2015-10-16 00:35:28 -0700

    reorganize to nav layout

--- ui.r
+++ ui.r
@@ -1,60 +1,68 @@
 library(shiny)
 
-shinyUI(fluidPage(
-      
-      br(),
-      
-      sidebarPanel(width=3,
+shinyUI(navbarPage("BGC",
                    
                    tags$style(type="text/css",
                               ".shiny-output-error { visibility: hidden; }",
                               ".shiny-output-error:before { visibility: hidden; }"
                    ),
                    
-                   imageOutput('logo', height="250px"),
-                   textInput("location", "Location", "Googleable name (in lower 48)"),
-                   numericInput('radius', 'Search radius (km)', 25, step=5, min=5),
-                   selectInput("comparison", "Vegetation hierarchy comparison", c("horizontal", "vertical")),
-                   hr(),
                    
-                   uiOutput("localtypes")
-      ),
-      
-      
-      
-      mainPanel(
-            tabsetPanel(type = "tab",
-                        tabPanel("Intro",
-                                 br(),
-                                 h2(p("Biogeoclim displays relationships between biotic, geographic, and climatic patterns in light of recent and projected climate change.")),
-                                 br(),
-                                 h3(p("To get started, type a location of interest in the contiguous US. Optionally, select a radius within which to search for ecosystem types, and a hierarchical level from the National Vegetation Classification system. Then click the tabs across the top to explore aspects of locally-important vegetation types.")),
-                                 br(),
-                                 h4(p("Climatic differences drive much of the geographic variation in the ecosystems around us. As these climates change, local species and habitats must either adapt to the new climate, migrate to stay within their baseline climate envelope, or be extripated. While the science of predicting future biogeographic responses to climate change is highly complex and uncertain, biogeoclim aims to help illuminate the underlying qualitative relationships between the dimensions of these systems."))
-                        ),
-                        tabPanel("Biotic",
-                                 br(),
-                                 h3(textOutput("biogeo_title")),
-                                 br(),
-                                 plotOutput('biogeo', height="500px"),
-                                 h3(textOutput("bioclim_title")),
-                                 br(),
-                                 plotOutput('bioclim', height="700px"),
-                                 numericInput('prob', 'Probability contour', .90, step=.01, min=.25, max=.99)), 
-                        tabPanel("Abiotic",
-                                 br(),
-                                 h3(textOutput("geoclim_title")),
-                                 br(),
-                                 fluidRow(
-                                       column(4, sliderInput('zoom', 'Zoom', 8, step=1, min=4, max=12)),
-                                       column(4, sliderInput('neighbors', 'Specificity', .05, step=.01, min=.01, max=.25)), 
-                                       column(4, selectInput("basemap", "Basemap", c("terrain", "satellite", "roadmap")))
-                                       
-                                 ),
-                                 fluidRow(
-                                       plotOutput('geoclim', height="600px")))
-                        
-            )
-            
-      )
+                   tabPanel("Intro",
+                            imageOutput('logo', height="250px"),
+                            textInput("location", "Location", "Googleable name (in lower 48)"),
+                            br(),
+                            h2(p("Biogeoclim displays relationships between biotic, geographic, and climatic patterns in light of recent and projected climate change.")),
+                            br(),
+                            h3(p("To get started, type a location of interest in the contiguous US. Optionally, select a radius within which to search for ecosystem types, and a hierarchical level from the National Vegetation Classification system. Then click the tabs across the top to explore aspects of locally-important vegetation types.")),
+                            br(),
+                            h4(p("Climatic differences drive much of the geographic variation in the ecosystems around us. As these climates change, local species and habitats must either adapt to the new climate, migrate to stay within their baseline climate envelope, or be extripated. While the science of predicting future biogeographic responses to climate change is highly complex and uncertain, biogeoclim aims to help illuminate the underlying qualitative relationships between the dimensions of these systems."))
+                   ),
+                   
+                   tabPanel("Biotic",
+                            h3(textOutput("biogeo_title")),
+                            br(),
+                            
+                            sidebarLayout(
+                                  sidebarPanel(width=3,
+                                        numericInput('radius', 'Search radius (km)', 25, step=5, min=5),
+                                        selectInput("comparison", "Vegetation hierarchy comparison", c("horizontal", "vertical")),
+                                        hr(),
+                                        uiOutput("localtypes"),
+                                        numericInput('prob', 'Probability contour', .90, step=.01, min=.25, max=.99)
+                                  ),
+                                  
+                                  mainPanel(
+                                        plotOutput('biogeo', height="500px"),
+                                        h3(textOutput("bioclim_title")),
+                                        br(),
+                                        plotOutput('bioclim', height="700px")
+                                  )
+                            )
+                            
+                   ), 
+                   
+                   tabPanel("Abiotic",
+                            h3(textOutput("geoclim_title")),
+                            br(),
+                            
+                            sidebarLayout(
+                                  sidebarPanel(width=3,
+                                        #imageOutput('logo', height="250px"),
+                                        sliderInput('zoom', 'Zoom', 
+                                                    8, step=1, min=4, max=12),
+                                        sliderInput('neighbors', 'Specificity', 
+                                                    .05, step=.01, min=.01, max=.25), 
+                                        selectInput("basemap", "Basemap", 
+                                                    c("terrain", "satellite", "roadmap"))
+                                        
+                                  ),
+                                  
+                                  mainPanel(
+                                        plotOutput("geoclim", height="600px"))
+                            )
+                   )
+                   
+                   
+                   
 ))
